cd41d61cf903bd6ce03725d5fa8e8ee0541ddf97
YoheiTerashima <yamanogomora@gmail.com>
2022-05-10 15:33:20 +0900

    #1 Creating Objects for bar chart and Controlling chart animation

--- AnimatedChartWithCSVData/main.py
+++ AnimatedChartWithCSVData/main.py
@@ -6,8 +6,13 @@ FILEPATH = "Please enter your file path"
 
 
 def clear_all():
+  obj.animation_data_clear()
   for obj in bpy.data.objects:
     bpy.data.objects.remove(obj)
+
+
+def clean_up():
+  bpy.ops.object.select_all(action='DESELECT')
 
 
 def read_file():
@@ -34,20 +39,52 @@ def read_file():
   return results
 
 
-def create_bar(data):
-  print(data)
+def add_keyframes(objects, data):
+  offset = 0
+  index = 0 
+
+  for obj in objects:
+    dic = data[index]
+    height = float(dic['average'])
+    
+    obj.scale = [1, 1, 0]
+    obj.keyframe_insert(data_path = "scale", frame = 1 + offset)
+
+    obj.scale = [1, 1, height]
+    obj.keyframe_insert(data_path = "scale", frame = 20 + offset)
+
+    index += 1
+    offset += 1
+
+
+def create_bar_object(index):
+  bpy.ops.mesh.primitive_cube_add(
+    size=1, 
+    enter_editmode=True, 
+    align='WORLD',
+    location=(0, 1 * index, 0)
+  )
+
+  bpy.ops.transform.translate(
+    value=(0, 0, 0.5)
+  )
+  bpy.ops.object.editmode_toggle()
+
 
 
 def create_chart(data):
   for i in range(len(data)):
-    create_bar(data[i])
+    create_bar_object(i)
 
 
 def main():
   data = read_file()
   create_chart(data)
+  items = bpy.data.collections["Collection"].objects
+  add_keyframes(objects=items, data=data)
+
   
 
 clear_all()
-
 main()
+clean_up()
